cef982c395855df4e725df9c95402950e2d32218
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-10-23 19:20:48 -0300

    diseño form

--- farmacia-residencia-modelado/Index.Designer.cs
+++ farmacia-residencia-modelado/Index.Designer.cs
@@ -28,7 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtLabo = new System.Windows.Forms.TextBox();
+            this.btn_Buscar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtCod
+            // 
+            this.txtCod.BackColor = System.Drawing.Color.Silver;
+            this.txtCod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCod.Location = new System.Drawing.Point(74, 69);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(110, 22);
+            this.txtCod.TabIndex = 0;
+            this.txtCod.Text = "Código";
+            // 
+            // txtNom
+            // 
+            this.txtNom.BackColor = System.Drawing.Color.Silver;
+            this.txtNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNom.Location = new System.Drawing.Point(262, 69);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(110, 22);
+            this.txtNom.TabIndex = 1;
+            this.txtNom.Text = "Nombre";
+            // 
+            // txtLabo
+            // 
+            this.txtLabo.BackColor = System.Drawing.Color.Silver;
+            this.txtLabo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLabo.Location = new System.Drawing.Point(444, 69);
+            this.txtLabo.Name = "txtLabo";
+            this.txtLabo.Size = new System.Drawing.Size(110, 22);
+            this.txtLabo.TabIndex = 2;
+            this.txtLabo.Text = "Laboratorio";
+            // 
+            // btn_Buscar
+            // 
+            this.btn_Buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Buscar.Location = new System.Drawing.Point(624, 69);
+            this.btn_Buscar.Name = "btn_Buscar";
+            this.btn_Buscar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Buscar.TabIndex = 3;
+            this.btn_Buscar.Text = "Consultar";
+            this.btn_Buscar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(679, 229);
+            this.dataGridView1.TabIndex = 4;
             // 
             // Index
             // 
@@ -36,16 +92,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Buscar);
+            this.Controls.Add(this.txtLabo);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.txtCod);
             this.MaximizeBox = false;
             this.Name = "Index";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Index";
+            this.Text = "Consultar Medicamento";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtLabo;
+        private System.Windows.Forms.Button btn_Buscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
