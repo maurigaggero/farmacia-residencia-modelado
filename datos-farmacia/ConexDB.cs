e347060eaf94182990f104eae2bfb8054c1d3587
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-10-23 19:28:07 -0300

    add proyecto datos y conexion db

---
+++ datos-farmacia/ConexDB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datos_farmacia
+{
+    public class ConexDb
+    {
+        public SqlConnection conexion;
+        public string cadenaConexion = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = clases_farmacia.DataContext; Integrated Security = True;";
+
+        #region CONSTRUCTOR
+        public ConexDb()
+        {
+            conexion = new SqlConnection(cadenaConexion);
+        }
+        #endregion
+
+        #region MÉTODOS
+        public void AbrirConexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Broken || conexion.State == ConnectionState.Closed)
+                    conexion.Open();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al abrir conexión", e);
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Open)
+                    conexion.Close();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al cerrar conexión", e);
+            }
+        }
+    }
+    #endregion
+}
