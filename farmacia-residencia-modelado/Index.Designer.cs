5a76ead29d9060912c5e78be4818004fcddd5294
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-10-24 18:20:28 -0300

    consulta por nombre de medicamento ok

--- farmacia-residencia-modelado/Index.Designer.cs
+++ farmacia-residencia-modelado/Index.Designer.cs
@@ -31,8 +31,17 @@
             this.txtCod = new System.Windows.Forms.TextBox();
             this.txtNom = new System.Windows.Forms.TextBox();
             this.btn_Buscar = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dg = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lbl_fecha = new System.Windows.Forms.Label();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Laboratorio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha_Venc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtCod
@@ -49,7 +58,7 @@
             // 
             this.txtNom.BackColor = System.Drawing.Color.Silver;
             this.txtNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNom.Location = new System.Drawing.Point(256, 69);
+            this.txtNom.Location = new System.Drawing.Point(246, 69);
             this.txtNom.Name = "txtNom";
             this.txtNom.Size = new System.Drawing.Size(110, 22);
             this.txtNom.TabIndex = 1;
@@ -57,23 +66,81 @@
             // 
             // btn_Buscar
             // 
-            this.btn_Buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Buscar.Location = new System.Drawing.Point(624, 69);
+            this.btn_Buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Buscar.Location = new System.Drawing.Point(656, 71);
             this.btn_Buscar.Name = "btn_Buscar";
             this.btn_Buscar.Size = new System.Drawing.Size(75, 23);
             this.btn_Buscar.TabIndex = 3;
             this.btn_Buscar.Text = "Consultar";
             this.btn_Buscar.UseVisualStyleBackColor = true;
+            this.btn_Buscar.Click += new System.EventHandler(this.btn_Buscar_Click);
             // 
-            // dataGridView1
+            // dg
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(52, 138);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(679, 229);
-            this.dataGridView1.TabIndex = 4;
+            this.dg.AllowUserToAddRows = false;
+            this.dg.AllowUserToDeleteRows = false;
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Nombre,
+            this.Laboratorio,
+            this.Unidad,
+            this.Cantidad,
+            this.Fecha_Venc});
+            this.dg.Location = new System.Drawing.Point(52, 138);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(679, 229);
+            this.dg.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Silver;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(589, 71);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(29, 21);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // lbl_fecha
+            // 
+            this.lbl_fecha.AutoSize = true;
+            this.lbl_fecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_fecha.ForeColor = System.Drawing.Color.White;
+            this.lbl_fecha.Location = new System.Drawing.Point(394, 73);
+            this.lbl_fecha.Name = "lbl_fecha";
+            this.lbl_fecha.Size = new System.Drawing.Size(189, 16);
+            this.lbl_fecha.TabIndex = 6;
+            this.lbl_fecha.Text = "Vencimiento dentro de meses:";
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Laboratorio
+            // 
+            this.Laboratorio.HeaderText = "Laboratorio";
+            this.Laboratorio.Name = "Laboratorio";
+            // 
+            // Unidad
+            // 
+            this.Unidad.HeaderText = "Unidad";
+            this.Unidad.Name = "Unidad";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // Fecha_Venc
+            // 
+            this.Fecha_Venc.HeaderText = "Fecha_Venc";
+            this.Fecha_Venc.Name = "Fecha_Venc";
             // 
             // Index
             // 
@@ -81,7 +148,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbl_fecha);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dg);
             this.Controls.Add(this.btn_Buscar);
             this.Controls.Add(this.txtNom);
             this.Controls.Add(this.txtCod);
@@ -90,7 +159,8 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consultar Medicamento";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,7 +171,15 @@
         private System.Windows.Forms.TextBox txtCod;
         private System.Windows.Forms.TextBox txtNom;
         private System.Windows.Forms.Button btn_Buscar;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dg;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lbl_fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Laboratorio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Unidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha_Venc;
     }
 }
 
--- farmacia-residencia-modelado/Index.cs
+++ farmacia-residencia-modelado/Index.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using datos_farmacia;
 
 namespace farmacia_residencia_modelado
 {
@@ -15,6 +16,42 @@ namespace farmacia_residencia_modelado
         public Index()
         {
             InitializeComponent();
+        }
+
+        private void LlenarDg(string cual)
+        {
+            DatosMedicamentos medic = new DatosMedicamentos();
+
+            dg.Rows.Clear();
+            if (cual == "Todos")
+            {
+                DataSet ds = new DataSet();
+                ds = medic.consultarMedicamento("todos");
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        dg.Rows.Add(dr[0], dr[1], dr[2], dr[3], dr[4], dr[5]);
+                    }
+                }
+            }
+            else
+            {
+                DataSet ds = new DataSet();
+                ds = medic.consultarMedicamento(cual);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        dg.Rows.Add(dr[0], dr[1], dr[2], dr[3], dr[4], dr[5]);
+                    }
+                }
+            }
+        }
+
+        private void btn_Buscar_Click(object sender, EventArgs e)
+        {
+            LlenarDg(txtNom.Text);
         }
     }
 }
