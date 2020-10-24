6ea57aee63dc00ac282df3b78d9b66a72aceaf23
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-10-24 17:58:48 -0300

    metodo filtra por fecha_ venc

--- datos-farmacia/DatosMedicamentos.cs
+++ datos-farmacia/DatosMedicamentos.cs
@@ -13,13 +13,50 @@ namespace datos_farmacia
         public DataSet consultarMedicamento(string cual)
         {
             string orden = string.Empty;
+
             if (cual != "todos")
-                orden = "select m.Id, m.Codigo, m.Nombre, m.Laboratorio, m.Fecha_Vencimiento, m.Unidad, i.Cantidad" +
-                         " from Medicamentos m, Inventario i " +
-                         "where m.Nombre = '" + (cual) +"'";
+                orden = "select m.Codigo, m.nombre, m.laboratorio, m.unidad, i.cantidad, i.Fecha_Vencimiento" +
+                         " from Medicamentos m, inventario i " +
+                         "where i.Codigo = m.Codigo and  m.Nombre like '%" + (cual) + "%'";
             else
                 orden = "select m.Id, m.Codigo, m.Nombre, m.Laboratorio, m.Fecha_Vencimiento, m.Unidad, i.Cantidad" +
-                         " from Medicamentos m, Inventario i ";
+                         " from Medicamentos m, Inventario i " +
+                         "where i.Codigo = m.Codigo";
+
+            SqlCommand cmd = new SqlCommand(orden, conexion);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            try
+            {
+                AbrirConexion();
+                cmd.ExecuteNonQuery();
+                da.SelectCommand = cmd;
+                da.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al listar registros", e);
+            }
+            finally
+            {
+                CerrarConexion();
+                cmd.Dispose();
+            }
+            return ds;
+        }
+
+        public DataSet consultarFecha(int meses)
+        {
+            string orden = string.Empty;
+            if (meses == 0)
+                orden = "select m.Id, m.Codigo, m.Nombre, m.Laboratorio, m.Fecha_Vencimiento, m.Unidad, i.Cantidad" +
+                         " from Medicamentos m, Inventario i " +
+                         "where i.Codigo = m.Codigo";
+            else
+                orden = "select m.Codigo, m.nombre, m.laboratorio, m.unidad, i.cantidad, i.Fecha_Vencimiento" +
+                         " from Medicamentos m, inventario i " +
+                         "where i.Codigo = m.Codigo and Fecha_Vencimiento BETWEEN getdate() AND dateadd(month,"+ +meses + ", getdate())";
 
             SqlCommand cmd = new SqlCommand(orden, conexion);
             DataSet ds = new DataSet();
