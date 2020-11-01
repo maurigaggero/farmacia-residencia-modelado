7a3257365da13d94117de677c9ec3ec43e8f5262
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-11-01 00:34:37 -0300

    filtro en simultaneo sql

--- datos-farmacia/DatosMedicamentos.cs
+++ datos-farmacia/DatosMedicamentos.cs
@@ -10,162 +10,78 @@ namespace datos_farmacia
 {
     public class DatosMedicamentos : ConexDb
     {
-        public DataSet consultarMedicamento(string cual)
+        public DataSet consultarMedicamento(string medicamento, string labo, string codigo, int tiempo)
         {
             string orden = string.Empty;
 
-            if (cual != string.Empty)
-                orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
-                         " from Medicamentos m, inventario i " +
-                         "where i.Codigo = m.Codigo and  m.Nombre like '%" + (cual) + "%' " +
-                         "order by m.nombre";
-            else
-                orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
-                         " from Medicamentos m, Inventario i " +
-                         "where i.Codigo = m.Codigo " +
-                          "order by m.nombre";
-
-            SqlCommand cmd = new SqlCommand(orden, conexion);
-            DataSet ds = new DataSet();
-            SqlDataAdapter da = new SqlDataAdapter();
-
-            try
-            {
-                AbrirConexion();
-                cmd.ExecuteNonQuery();
-                da.SelectCommand = cmd;
-                da.Fill(ds);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Error al listar registros", e);
-            }
-            finally
-            {
-                CerrarConexion();
-                cmd.Dispose();
-            }
-            return ds;
-        }
-
-        public DataSet consultarLaboratorio(string cual)
-        {
-            string orden = string.Empty;
-
-            if (cual != string.Empty)
-                orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
-                         " from Medicamentos m, inventario i " +
-                         "where i.Codigo = m.Codigo and  m.Laboratorio like '%" + (cual) + "%' " +
-                         "order by m.Laboratorio";
-            else
-                orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
-                         " from Medicamentos m, Inventario i " +
-                         "where i.Codigo = m.Codigo " +
-                          "order by m.nombre";
-
-            SqlCommand cmd = new SqlCommand(orden, conexion);
-            DataSet ds = new DataSet();
-            SqlDataAdapter da = new SqlDataAdapter();
-
-            try
-            {
-                AbrirConexion();
-                cmd.ExecuteNonQuery();
-                da.SelectCommand = cmd;
-                da.Fill(ds);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Error al listar registros", e);
-            }
-            finally
-            {
-                CerrarConexion();
-                cmd.Dispose();
-            }
-            return ds;
-        }
-
-        public DataSet consultarVencimiento(int tiempo)
-        {
-            string orden = string.Empty;
             switch (tiempo)
             {
+                case -1:
+                    orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
+                            " from Medicamentos m, inventario i " +
+                            "where i.Codigo = m.Codigo and m.Nombre like '%" + (medicamento) + "%' " +
+                            "and m.Laboratorio like '%" + (labo) + "%' " +
+                            "and m.Codigo like '%" + (codigo) + "%' " +
+                            " order by i.Fecha_Vencimiento asc";
+                    break;
+
                 case 0:
-                    orden = "select m.Codigo, m.nombre, m.laboratorio, m.unidad, i.cantidad, i.Fecha_Vencimiento" +
-                         " from Medicamentos m, Inventario i " +
-                         "where i.Codigo = m.Codigo " +
-                         "order by i.Fecha_Vencimiento asc";
+                    orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
+                            " from Medicamentos m, inventario i " +
+                            "where i.Codigo = m.Codigo and m.Nombre like '%" + (medicamento) + "%' " +
+                            "and m.Laboratorio like '%" + (labo) + "%' " +
+                            "and m.Codigo like '%" + (codigo) + "%' " +
+                            " order by i.Fecha_Vencimiento asc";
                     break;
+
                 case 1:
-                    orden = "select m.Codigo, m.nombre, m.laboratorio, m.unidad, i.cantidad, i.Fecha_Vencimiento" +
-                          " from Medicamentos m, inventario i " +
-                          "where i.Codigo = m.Codigo and Fecha_Vencimiento BETWEEN dateadd(month," + -6 + ", getdate()) AND  getdate() " +
-                          "order by i.Fecha_Vencimiento asc";
+                    orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
+                            " from Medicamentos m, inventario i " +
+                            "where i.Codigo = m.Codigo and m.Nombre like '%" + (medicamento) + "%' " +
+                            "and m.Laboratorio like '%" + (labo) + "%' " +
+                            "and m.Codigo like '%" + (codigo) + "%' " +
+                            "and Fecha_Vencimiento BETWEEN dateadd(month, " + -6 + ", getdate()) AND getdate() " +
+                            " order by i.Fecha_Vencimiento asc";
                     break;
+
                 case 2:
-                    orden = "select m.Codigo, m.nombre, m.laboratorio, m.unidad, i.cantidad, i.Fecha_Vencimiento" +
-                          " from Medicamentos m, inventario i " +
-                          "where i.Codigo = m.Codigo and Fecha_Vencimiento BETWEEN getdate() AND dateadd(week," + +1 + ", getdate()) " +
-                          "order by i.Fecha_Vencimiento asc";
+                    orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
+                            " from Medicamentos m, inventario i " +
+                            "where i.Codigo = m.Codigo and m.Nombre like '%" + (medicamento) + "%' " +
+                            "and m.Laboratorio like '%" + (labo) + "%' " +
+                            "and m.Codigo like '%" + (codigo) + "%' " +
+                            "and Fecha_Vencimiento BETWEEN getdate() AND dateadd(week," + +1 + ", getdate()) " +
+                            " order by i.Fecha_Vencimiento asc";
                     break;
                 case 3:
-                    orden = "select m.Codigo, m.nombre, m.laboratorio, m.unidad, i.cantidad, i.Fecha_Vencimiento" +
-                          " from Medicamentos m, inventario i " +
-                          "where i.Codigo = m.Codigo and Fecha_Vencimiento BETWEEN getdate() AND dateadd(month," + +1 + ", getdate()) " +
-                          "order by i.Fecha_Vencimiento asc";
+                    orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
+                            " from Medicamentos m, inventario i " +
+                            "where i.Codigo = m.Codigo and m.Nombre like '%" + (medicamento) + "%' " +
+                            "and m.Laboratorio like '%" + (labo) + "%' " +
+                            "and m.Codigo like '%" + (codigo) + "%' " +
+                            "and Fecha_Vencimiento BETWEEN getdate() AND dateadd(month," + +1 + ", getdate()) " +
+                            " order by i.Fecha_Vencimiento asc";
                     break;
                 case 4:
-                    orden = "select m.Codigo, m.nombre, m.laboratorio, m.unidad, i.cantidad, i.Fecha_Vencimiento" +
-                          " from Medicamentos m, inventario i " +
-                          "where i.Codigo = m.Codigo and Fecha_Vencimiento BETWEEN dateadd(month," + +1 + ", getdate()) AND dateadd(yy, datediff(yy, 0, getdate()) + 1, -1) " +
-                          "order by i.Fecha_Vencimiento asc";
+                    orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
+                            " from Medicamentos m, inventario i " +
+                            "where i.Codigo = m.Codigo and m.Nombre like '%" + (medicamento) + "%' " +
+                            "and m.Laboratorio like '%" + (labo) + "%' " +
+                            "and m.Codigo like '%" + (codigo) + "%' " +
+                            "and Fecha_Vencimiento BETWEEN dateadd(month, " + +1 + ", getdate()) AND dateadd(yy, datediff(yy, 0, getdate()) +1, -1) " +
+                            " order by i.Fecha_Vencimiento asc";
+
                     break;
                 case 5:
-                    orden = "select m.Codigo, m.nombre, m.laboratorio, m.unidad, i.cantidad, i.Fecha_Vencimiento" +
-                          " from Medicamentos m, inventario i " +
-                          "where i.Codigo = m.Codigo and Fecha_Vencimiento BETWEEN dateadd(yy, datediff(yy, 0, getdate()) + 1, 0) AND dateadd(yy, datediff(yy, 0, getdate()) + 2, -1) " +
-                          "order by i.Fecha_Vencimiento asc";
+                    orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
+                            " from Medicamentos m, inventario i " +
+                            "where i.Codigo = m.Codigo and m.Nombre like '%" + (medicamento) + "%' " +
+                            "and m.Laboratorio like '%" + (labo) + "%' " +
+                            "and m.Codigo like '%" + (codigo) + "%' " +
+                            "and Fecha_Vencimiento BETWEEN dateadd(yy, datediff(yy, 0, getdate()) + 1, 0) AND dateadd(yy, datediff(yy, 0, getdate()) + 2, -1) " +
+                            " order by i.Fecha_Vencimiento asc";
                     break;
             }
-
-            SqlCommand cmd = new SqlCommand(orden, conexion);
-            DataSet ds = new DataSet();
-            SqlDataAdapter da = new SqlDataAdapter();
-
-            try
-            {
-                AbrirConexion();
-                cmd.ExecuteNonQuery();
-                da.SelectCommand = cmd;
-                da.Fill(ds);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Error al listar registros", e);
-            }
-            finally
-            {
-                CerrarConexion();
-                cmd.Dispose();
-            }
-            return ds;
-        }
-
-        public DataSet consultarCodigo(string codigo)
-        {
-            string orden = string.Empty;
-
-            if (codigo == string.Empty)
-                orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
-                         " from Medicamentos m, inventario i " +
-                         "where i.Codigo = m.Codigo " +
-                         "order by m.nombre";
-            else
-                orden = "select m.Codigo, m.Nombre, m.Laboratorio, m.Unidad, i.Cantidad, i.Fecha_Vencimiento" +
-                         " from Medicamentos m, Inventario i " +
-                         "where i.Codigo = m.Codigo and m.Codigo like '%" + codigo + "%' " +
-                          "order by m.nombre";
 
             SqlCommand cmd = new SqlCommand(orden, conexion);
             DataSet ds = new DataSet();
--- farmacia-residencia-modelado/Index.cs
+++ farmacia-residencia-modelado/Index.cs
@@ -22,80 +22,10 @@ namespace farmacia_residencia_modelado
         }
 
         #region METODOS
-        private void LlenarDgNombre(string cual)
+        private void LlenarDg(string medicamento, string labo, string codigo, int tiempo)
         {
             dg.Rows.Clear();
-            if (cual == string.Empty)
-            {
-                ds = medic.consultarMedicamento("");
-            }
-            else
-            {
-                ds = medic.consultarMedicamento(cual);
-            }
-
-            if (ds.Tables[0].Rows.Count > 0)
-            {
-                foreach (DataRow dr in ds.Tables[0].Rows)
-                {
-                    dg.Rows.Add(dr[0], dr[1], dr[2], dr[3], dr[4], dr[5]);
-                }
-            }
-        }
-
-        private void LlenarDgVencimiento(int meses)
-        {
-            dg.Rows.Clear();
-            if (meses == 0)
-            {
-                ds = medic.consultarVencimiento(0);
-            }
-            else
-            {
-                ds = medic.consultarVencimiento(meses);
-            }
-
-            if (ds.Tables[0].Rows.Count > 0)
-            {
-                foreach (DataRow dr in ds.Tables[0].Rows)
-                {
-                    dg.Rows.Add(dr[0], dr[1], dr[2], dr[3], dr[4], dr[5]);
-                }
-            }
-        }
-
-        private void LlenarDgCodigo(string codigo)
-        {
-            dg.Rows.Clear();
-            if (codigo == "")
-            {
-                ds = medic.consultarCodigo("");
-            }
-            else
-            {
-                ds = medic.consultarCodigo(codigo);
-            }
-
-            if (ds.Tables[0].Rows.Count > 0)
-            {
-                foreach (DataRow dr in ds.Tables[0].Rows)
-                {
-                    dg.Rows.Add(dr[0], dr[1], dr[2], dr[3], dr[4], dr[5]);
-                }
-            }
-        }
-
-        private void LlenarDgLabo(string cual)
-        {
-            dg.Rows.Clear();
-            if (cual == "")
-            {
-                ds = medic.consultarLaboratorio("");
-            }
-            else
-            {
-                ds = medic.consultarLaboratorio(cual);
-            }
+            ds = medic.consultarMedicamento(medicamento, labo, codigo, tiempo);
 
             if (ds.Tables[0].Rows.Count > 0)
             {
@@ -110,30 +40,23 @@ namespace farmacia_residencia_modelado
         #region EVENTOS
         private void txtNom_TextChanged(object sender, EventArgs e)
         {
-            LlenarDgNombre(txtNom.Text);
+            LlenarDg(txtNom.Text, txt_labo.Text, txt_codigo.Text,combo_venc.SelectedIndex);
         }
 
         private void combo_venc_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (combo_venc.SelectedIndex != -1)
-            {
-                LlenarDgVencimiento(combo_venc.SelectedIndex);
-            }
-            else
-            {
-
-            }
+            LlenarDg(txtNom.Text, txt_labo.Text, txt_codigo.Text, combo_venc.SelectedIndex);
         }
 
         private void txt_codigo_TextChanged(object sender, EventArgs e)
         {
-            LlenarDgCodigo(txt_codigo.Text);
+            LlenarDg(txtNom.Text, txt_labo.Text, txt_codigo.Text, combo_venc.SelectedIndex);
         }
         #endregion
 
         private void txt_labo_TextChanged(object sender, EventArgs e)
         {
-            LlenarDgLabo(txt_labo.Text);
+            LlenarDg(txtNom.Text, txt_labo.Text, txt_codigo.Text, combo_venc.SelectedIndex);
         }
     }
 }
